a4606ef1a3be2645afdc8471ac125212debea76d
任意 <UniversalQuantifier@outlook.com>
2022-01-06 19:05:12 +0800

    Step 2: Adding a Library

---
+++ Step1/MathFunctions/mysqrt.cc
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <cmath>
+double mysqrt(double inputValue) {
+	std::cout << "this is in the function of mysqrt" << std::endl;
+	return sqrt(inputValue);
+}
--- Step1/tutorial.cc
+++ Step1/tutorial.cc
@@ -3,6 +3,9 @@
 #include <cmath>
 #include <string>
 #include <TutorialConfig.h>
+#ifdef USE_MYMATH	// 要定义在TutorialConfig.h的下面，不然没有USE_MYMATH宏
+#  include "MathFunctions.h"
+#endif
 
 int main(int argc, char *argv[]) {
 	if (argc < 2) {
@@ -15,7 +18,11 @@ int main(int argc, char *argv[]) {
 	if (argc == 2) {
 		//const double inputValue = atof(argv[1]);
 		const double inputValue = std::stod(argv[1]);
-		std::cout << sqrt(inputValue) << std::endl;
+		#ifdef USE_MYMATH
+			std::cout << mysqrt(inputValue) << std::endl;
+		#else
+			std::cout << sqrt(inputValue) << std::endl;
+		#endif
 	}
 	return 0;
 }
