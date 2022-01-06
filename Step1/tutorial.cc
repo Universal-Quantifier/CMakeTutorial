c08aefc44ff8fc24ba0ef3c562245748c30891f6
任意 <UniversalQuantifier@outlook.com>
2022-01-06 16:59:41 +0800

    Step 1: A Basic Starting Point

---
+++ Step1/tutorial.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+//#include <cstdlib>
+#include <cmath>
+#include <string>
+#include <TutorialConfig.h>
+
+int main(int argc, char *argv[]) {
+	if (argc < 2) {
+		// report version
+		std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
+				  << Tutorial_VERSION_MINOR << std::endl;
+		std::cout << "Usage: " << argv[0] << " number" << std::endl;
+		return 1;
+	}
+	if (argc == 2) {
+		//const double inputValue = atof(argv[1]);
+		const double inputValue = std::stod(argv[1]);
+		std::cout << sqrt(inputValue) << std::endl;
+	}
+	return 0;
+}
