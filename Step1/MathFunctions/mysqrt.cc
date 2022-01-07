efc54eb8c2d6a768ed00cd62907d73509a8967b7
任意 <UniversalQuantifier@outlook.com>
2022-01-07 13:06:49 +0800

    Step 5: Adding System Introspection

--- Step1/MathFunctions/mysqrt.cc
+++ Step1/MathFunctions/mysqrt.cc
@@ -2,5 +2,11 @@
 #include <cmath>
 double mysqrt(double inputValue) {
 	//std::cout << "this is in the function of mysqrt" << std::endl;
-	return sqrt(inputValue);
+	#if defined(HAVE_LOG) && defined(HAVE_EXP)
+		std::cout << "Computing sqrt of " << inputValue << " to be " << exp(log(inputValue) * 0.5)
+            << " using log and exp" << std::endl;
+		return exp(log(inputValue) * 0.5);
+	#else
+		return sqrt(inputValue);
+	#endif
 }
