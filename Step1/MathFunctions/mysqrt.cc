cf2d0797dfde7b6a96763fa540d893ecb4122072
任意 <UniversalQuantifier@outlook.com>
2022-01-07 13:30:28 +0800

    Step 6: Adding a Custom Command and Generated File

--- Step1/MathFunctions/mysqrt.cc
+++ Step1/MathFunctions/mysqrt.cc
@@ -1,12 +1,27 @@
 #include <iostream>
-#include <cmath>
+//#include <cmath>
+#include <Table.h>
 double mysqrt(double inputValue) {
-	//std::cout << "this is in the function of mysqrt" << std::endl;
-	#if defined(HAVE_LOG) && defined(HAVE_EXP)
-		std::cout << "Computing sqrt of " << inputValue << " to be " << exp(log(inputValue) * 0.5)
-            << " using log and exp" << std::endl;
-		return exp(log(inputValue) * 0.5);
-	#else
-		return sqrt(inputValue);
-	#endif
+	if (inputValue <= 0) {
+    return 0;
+  }
+
+  // use the table to help find an initial value
+  double result = inputValue;
+  if (inputValue >= 1 && inputValue < 10) {
+    std::cout << "Use the table to help find an initial value " << std::endl;
+    result = sqrtTable[static_cast<int>(inputValue)];
+  }
+
+  // do ten iterations
+  for (int i = 0; i < 10; ++i) {
+    if (result <= 0) {
+      result = 0.1;
+    }
+    double delta = inputValue - (result * result);
+    result = result + 0.5 * delta / result;
+    std::cout << "Computing sqrt of " << inputValue << " to be " << result << std::endl;
+  }
+
+  return result;
 }
